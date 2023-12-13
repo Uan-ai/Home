e132c3979653859d93ea950380dbc6e0204ef51b
burhans_1140 <78636472+Uan-ai@users.noreply.github.com>
2023-12-13 13:26:30 +0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,21 @@
+nama = ['Burhan']
+let menuicn = document.querySelector(".menuicn");
+let nav = document.querySelector(".navcontainer");
+ 
+menuicn.addEventListener("click", () => {
+    nav.classList.toggle("navclose");
+})
+
+let query = document.querySelector('.query');
+    let searchbtn = document.querySelector('.searchbtn')
+    searchbtn.onclick = function(){
+        let url ='file:///D:/burhan/Tugas%20akhir/index.html#'+query.value;
+        window.open(url,'_self');
+    }
+
+    let query2 = document.querySelector('#query2');
+    let search2 = document.querySelector('#search2')
+    search2.onclick = function(){
+        let url ='file:///D:/burhan/Tugas%20akhir/index.html#'+query2.value;
+        window.open(url,'_self');
+    }
